f943f0a13a08efe4aa1d9685612cd00e6421c786
oluwatumininu01 <adekoyaoluwatumininu01@gmail.com>
2023-07-20 18:39:17 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}

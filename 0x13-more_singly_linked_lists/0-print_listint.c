9a86ac05a7362d1c53cfac42f364a06d3fc7967a
oluwatumininu01 <adekoyaoluwatumininu01@gmail.com>
2023-08-30 13:02:08 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,19 +2,19 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * @p: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *p)
 {
 	size_t num = 0;
 
-	while (h)
+	while (p)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", p->n);
 		num++;
-		h = h->next;
+		p = p->next;
 	}
 
 	return (num);
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,7 +30,7 @@ typedef struct listp_s
 	struct listp_s *next;
 } listp_t;
 
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *p);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
